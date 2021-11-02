5004118be35a9e245436077067e76b70767de50a
Kemal Ozer <mkemalozerr@gmail.com>
2021-11-02 12:40:31 +0300

    Proje oluşturuldu.

---
+++ csharp-pratikler/console-deneme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
