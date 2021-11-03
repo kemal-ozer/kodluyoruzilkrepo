a1242d77bffd9d61e325fcab8352be9b75bd7332
Kemal Ozer <mkemalozerr@gmail.com>
2021-11-03 14:26:59 +0300

    çalışılmış program.

---
+++ csharp-pratikler/degiskenler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 ="x";
+            object o2 ='y';
+            object o3 =3;
+            object o4 = 2.36;
+
+            // Değişken dönüşümleri
+            string s ="20";
+            int i = 20;
+
+            string yeniDeger = s + i.ToString();
+
+            Console.WriteLine(yeniDeger);
+
+            int i1 = i + Convert.ToInt32(s);
+            Console.WriteLine(i1);  //çıktısı 40 
+            
+        }
+    }
+}
