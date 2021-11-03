00df2190cc044e84a341ad7be10fafd39e178466
Kemal Ozer <mkemalozerr@gmail.com>
2021-11-03 13:40:40 +0300

    added.

---
+++ csharp-pratikler/degiskenler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            object o1 ="x";
+            object o2 ='y';
+            object o3 =3;
+            object o4 = 2.36;
+
+            // Değişken dönüşümleri
+            string s ="20";
+            int i = 20;
+
+            string yeniDeger = s + i.ToString();
+
+            Console.WriteLine(yeniDeger);
+
+        }
+    }
+}
