6230b267c1717151bb64dcd82a5a9f9f6e1c0795
Kemal Ozer <mkemalozerr@gmail.com>
2021-11-03 14:31:46 +0300

    add

--- csharp-pratikler/degiskenler/Program.cs
+++ csharp-pratikler/degiskenler/Program.cs
@@ -6,25 +6,10 @@ namespace degiskenler
     {
         static void Main(string[] args)
         {
-            DateTime dt = DateTime.Now;
-            Console.WriteLine(dt);
+            int degisken = 5;
+            string degisken ="merhaba";
+            Console.WriteLine(degisken);
 
-            object o1 ="x";
-            object o2 ='y';
-            object o3 =3;
-            object o4 = 2.36;
-
-            // Değişken dönüşümleri
-            string s ="20";
-            int i = 20;
-
-            string yeniDeger = s + i.ToString();
-
-            Console.WriteLine(yeniDeger);
-
-            int i1 = i + Convert.ToInt32(s);
-            Console.WriteLine(i1);  //çıktısı 40 
-            
         }
     }
 }
